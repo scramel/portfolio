66a5e7e26a76da61f40ecb216c125652749357ce
Scramel <scratchxmelody@gmail.com>
2024-05-23 02:18:20 -0400

    deploy

---
+++ js/about.3bdd616f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkscramel"]=self["webpackChunkscramel"]||[]).push([[594],{9220:function(n,s,t){t.r(s),t.d(s,{default:function(){return c}});var u=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],a=t(1656),l={},r=(0,a.A)(l,u,e,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.3bdd616f.js.map
